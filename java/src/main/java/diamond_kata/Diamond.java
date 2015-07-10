6b0778f99147bb74a61095686e299203c34b3f33
Diego Lemos <diegolemosderesende@gmail.com>
2015-07-10 16:42:36 +0200

    Design Diamond#draw mechanics.

--- java/src/main/java/diamond_kata/Diamond.java
+++ java/src/main/java/diamond_kata/Diamond.java
@@ -7,6 +7,25 @@ public class Diamond {
     }
 
     public String draw(char letter) {
+        char[][] slab = buildSlab(letter);
+        char[] columnFilter = createColumnFilter(letter);
+        char[][] diamond = filter(slab, columnFilter);
+        return stringify(diamond);
+    }
+
+    private String stringify(char[][] diamond) {
+        throw new UnsupportedOperationException("not implemented");
+    }
+
+    private char[][] filter(char[][] slap, char[] columnFilter) {
+        throw new UnsupportedOperationException("not implemented");
+    }
+
+    private char[] createColumnFilter(char letter) {
+        throw new UnsupportedOperationException("not implemented");
+    }
+
+    private char[][] buildSlab(char letter) {
         throw new UnsupportedOperationException("not implemented");
     }
 }
